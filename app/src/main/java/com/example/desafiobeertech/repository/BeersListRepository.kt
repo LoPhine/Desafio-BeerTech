d6114335d15ed4b569352e8e3a05aa8a58dd828a
Louise Josephine <loophinesj@gmail.com>
2020-11-12 00:59:10 -0300

    adding repository

---
+++ app/src/main/java/com/example/desafiobeertech/repository/BeersListRepository.kt
@@ -0,0 +1,35 @@
+package com.example.desafiobeertech.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.desafiobeertech.network.Beers
+import com.example.desafiobeertech.network.BeersApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class BeersListRepository {
+    private val beersListResponse = MutableLiveData<List<Beers>>()
+
+    val  cocktailList: LiveData<List<Beers>>
+        get() = beersListResponse
+
+    init {
+        getBeersList()
+    }
+
+    private fun getBeersList() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val listResult = BeersApi.retrofitService.getBeers().BeersList
+                beersListResponse.postValue(listResult)
+
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    beersListResponse.postValue(listOf())
+                }
+            }
+        }
+    }
+}
